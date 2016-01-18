40dc3b505bc039f9773b158e9f7266485f503995
Marica Odagaki <ento.entotto@gmail.com>
2016-01-18 15:37:58 -0800

    Fix and add more model data check

--- spec/controllers/application_controller_spec.rb
+++ spec/controllers/application_controller_spec.rb
@@ -4,11 +4,20 @@ describe Localdoc::ApplicationController do
   it "GET /" do
     get :show, use_route: :localdoc
     expect(response).to be_success
+    expect(assigns(:page_data)).to(
+      include(
+        :markdownOptions,
+        model: include(
+          :allDocs,
+          :filePath,
+          :savePath,
+          :editable,
+          :blockingError)))
   end
 
   it "GET /path/to/non-existent-doc.json" do
     get :show, path: "path/to/non-existent-doc.json", use_route: :localdoc
     expect(response).to be_success
-    expect(assigns(:blocking_error)).to include("File Not Found")
+    expect(assigns(:page_data)).to include(model: include(blockingError: "File Not Found"))
   end
 end
