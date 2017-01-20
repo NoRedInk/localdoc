69174134d16cd599237f4078e936cb73ae290645
Michael Glass <me@mike.is>
2017-01-20 13:58:38 +0100

    safetycolons is removed in later versions of sprockets

--- lib/localdoc/engine.rb
+++ lib/localdoc/engine.rb
@@ -8,12 +8,6 @@ module Localdoc
     isolate_namespace Localdoc
     engine_name 'localdoc'
 
-    initializer "localdoc.assets.configure" do |app|
-      app.config.assets.configure do |env|
-        env.unregister_postprocessor 'application/javascript', Sprockets::SafetyColons
-      end
-    end
-
     initializer "localdoc.assets.precompile" do |app|
       app.config.assets.precompile += %w(localdoc/bundle.js)
     end
--- localdoc.gemspec
+++ localdoc.gemspec
@@ -16,7 +16,7 @@ Gem::Specification.new do |s|
   s.files = Dir["{app,config,db,lib}/**/*"] + ["LICENSE", "Rakefile", "README.md"]
   s.test_files = Dir["test/**/*"]
 
-  s.add_dependency "rails", ">= 3.2.22"
+  s.add_dependency "rails", ">= 4.0.0"
   s.add_dependency "haml-rails", ">= 0.4.0"
   s.add_dependency "rake"
 
